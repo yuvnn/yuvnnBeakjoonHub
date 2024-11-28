674825cf8436a8fe365166f97436c2b2b5abb989
yuuveen <119295341+yuvnn@users.noreply.github.com>
2024-11-28 20:07:17 +0900

    [Silver II] Title: 유기농 배추, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,31 @@
-from collections import deque
-from sys import stdin
-input = stdin.readline
-
 t = int(input())
 for _ in range(t):
-    ans = 0
-    vis = []
-    Cabbage = []
     m,n,k = map(int,input().split())
+    arr = set()
+    ans = 0
     for _ in range(k):
         i,j = map(int,input().split())
-        Cabbage.append((j,i))
-    
-    def find(start):
-        global ans
-        vis.append(start)
-        stack = [start]
-        
-        while stack:
-            cur = stack.pop()
-            diff_x,diff_y= [0,0,1,-1],[1,-1,0,0]
-            for i in range(4):
-                diff_cur = (cur[0]+diff_x[i], cur[1]+diff_y[i])
-                if diff_cur in Cabbage and diff_cur not in vis:
-                    stack.append(diff_cur)
-                    vis.append(diff_cur)
-        
-        ans += 1
-                
-    for c in Cabbage:
-        if c not in vis:
-            find(c)
+        arr.add((i,j))
 
+    def find(cur):
+        global ans
+        stack = [cur]
+        while stack:
+            nx,ny = stack.pop()
+            dir = [(0,1),(0,-1),(1,0),(-1,0)]
+            for dx,dy in dir:
+                x,y = nx+dx,ny+dy
+                if (x,y) in arr:
+                    arr.remove((x,y))
+                    stack.append((x,y))
+        ans +=1
+    
+    while arr:
+        find(arr.pop())
+    
     print(ans)
+
+        
+
+
+
